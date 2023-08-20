dccf93d79f1f834cfebd096e2147198ee3898c30
Doug <nineafterten@gmail.com>
2023-08-19 23:05:50 -0700

    Generate selects pokemon for the team by random types; exclude dupes

--- pokemon-team-builder/src/App.vue
+++ pokemon-team-builder/src/App.vue
@@ -3,34 +3,73 @@
     data: () => ({
       pokedex: [],
       tempTeamLimit: 6,
-      tempTypeList: [],
+      currentTeamList: [],
       tempRegionList: [],
       typeList: ['normal', 'grass', 'water', 'fire', 'electric', 'ground', 'flying', 'rock', 'bug', 'ghost', 'poison', 'fighting', 'psychic', 'ice', 'dragon', 'steel', 'dark', 'fairy'],
       regionList: ['kanto', 'johto', 'hoenn', 'sinnoh', 'unova', 'kalos', 'alola', 'galar', 'paldea']
     }),
     methods: {
       capitalize: function(string) {
+        if (!string) { return '' }
         const firstChar = string[0].toUpperCase();
         const remainder = string.slice(1);
         return firstChar + remainder;
       },
-      generateTeam: function() {
-        this.getRandomTypes();
+      choosePokemonByType: function(type) {
+        console.log(`looking for ${type} types`);
+        const typedPokemon = this.pokedex.filter(function(mon) {
+          return mon.types[0]?.type?.name === type || mon.types[1]?.type?.name === type;
+        });
+        console.log(`found these for ${type} types`, typedPokemon);
+        return typedPokemon.length ? typedPokemon[Math.floor(Math.random() * typedPokemon.length)] : {};
       },
-      getRandomTypes: function() {
+      // TODO: Probably supposed to be a component with a slot? Need to research...
+      createTypeBadges: function(types) {
+        types = Array.isArray(types) ? types : [];
+        let template = '';
+        types.forEach(function(type) {
+          template += `<span class="type_${type.type.name}">${type.type.name}</span>`;
+        })
+        return template;
+      },
+      generateTeam: function() {
         let n;
-        this.tempTypeList = [];
+        this.currentTeamList = [];
+        // TODO: make the type filters pre-filter the list so we don't pick missing types
+        // TODO: change this to do/while and increment when we're sure we found a matching type (they can turn off types)
         for(var a=0; a < this.tempTeamLimit; a++) {
           n = Math.floor(Math.random() * this.typeList.length);
-          this.tempTypeList.push(this.typeList[n])
+          let newChoice = this.choosePokemonByType(this.typeList[n]);
+          let currentTeammate = this.currentTeamList.find(function(mon) { return mon.name === newChoice.name });
+          if (newChoice) {
+            if (currentTeammate) {
+              console.log(`Skipping ${ newChoice.name} - they are already on the team!`);
+            } else {
+              this.currentTeamList.push(newChoice);
+            }
+          }
         }
-      },
-      filterPokemonListByType: function(type) {
-        return '';
+        console.log('chosen team', this.currentTeamList);
       },
       async fetchPokemon() {
-        const initialRecord = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1', { cache: "force-cache" }).then((response) => response.json())
-        this.pokedex = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${initialRecord.count}`, { cache: "force-cache" }).then((response) => response.json())
+        const initialRecord = {count: 50}; // for debugging until we get the caching stored
+        //const initialRecord = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1', { cache: "force-cache" }).then((response) => response.json())
+        // get the full list by the current total count
+        await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${initialRecord.count}`, { cache: "force-cache" })
+          .then(response => response.json())
+          .then(response => {
+            response.results.forEach((pokemon) => {
+              // get the details for each pokemon so we can sort/filter by type, region, etc.
+              fetch(`${pokemon.url}`, { cache: "force-cache" })
+                .then(details => details.json())              
+                .then((details) => {
+                  this.pokedex.push(Object.assign( {}, pokemon, details));
+              })
+            })
+          })
+          .catch((err) => {
+            console.error(err);
+          })
       }
     },
     beforeCreate() {
@@ -43,11 +82,8 @@
   }
 
 /* NOTES
-  https://pokeapi.co/api/v2/pokemon -> .count
-  https://pokeapi.co/api/v2/pokemon/1
-
-  1 - fetch the count, get the full list of names
-  2 - for each pokemon, fetch the detail record on the corresponding url (cache it)
+  1 - ensure caching and or do local storage (endpoint spamming right now for testing)
+  2 - add Tailwind CSS for styling?
   3 - add 'ban' option for pokemon user doesn't like (exclusion list)
   4 - sort/filter by element type
   5 - sort/fiter by region
@@ -64,10 +100,6 @@
   </header>
 
   <main>
-    <button @click="fetchPokemon">Fetch Pokemon</button>
-    <div class="scroll-container">
-      <pre>{{ pokedex }}</pre>
-    </div>
     <h2>Team Size</h2>
     <select v-model="tempTeamLimit">
       <option>2</option>
@@ -87,18 +119,13 @@
 
     <button @click="generateTeam">Generate Pokemon Team</button>
 
-    <p v-if="!tempTypeList.length">No team generated yet.</p>
+    <p v-if="!currentTeamList.length">No team generated yet.</p>
     <ul v-else>
-      <li v-for="(temp, index) in tempTypeList" :key="`temp-type-${index}`">{{ this.capitalize(temp) }}</li>
+      <!-- make this a "slot" for a card? -->
+      <li v-for="(member, index) in currentTeamList" :key="`member-${index}`">{{ this.capitalize(member?.name) }} {{ this.createTypeBadges(member?.types) }}</li>
     </ul>
   </main>
 </template>
 
 <style scoped>
-.scroll-container {
-  height: 300px;
-  overflow: scroll;
-  outline: 1px solid gray;
-  background: #ccc;  
-}
 </style>
